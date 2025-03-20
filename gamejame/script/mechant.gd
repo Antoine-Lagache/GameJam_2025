664d7b821ea1ad4278316c8798a2be21e0b24586
azzayu <armand.guillet@gmail.com>
2025-03-20 17:51:06 +0100

    better

--- gamejame/script/mechant.gd
+++ gamejame/script/mechant.gd
@@ -8,7 +8,6 @@ func _ready():
 	position = Vector2(500, 250)
 	
 func _physics_process(delta: float) -> void:
-	print(len(get_children()))
 	pass
 
 
@@ -18,5 +17,5 @@ func _on_timer_timeout() -> void:
 		new_bullet.global_position = global_position
 		new_bullet.speed = randf_range(5, 10)
 		new_bullet.direction = randf_range(0, 2 * PI)
-		new_bullet.angular_velocity = randf_range(-0.1, 0.1)
+		new_bullet.angular_velocity = 0.01
 		add_child(new_bullet)
