49d21e4d892ffef563bf653b22dcd595ad4b5056
Lagache <antoine.lagache13@gmail.com>
2025-03-20 16:57:18 +0100

    z

--- gamejame/script/player.gd
+++ gamejame/script/player.gd
@@ -2,11 +2,33 @@ extends CharacterBody2D
 
 
 const SPEED = 300.0
+const start_position = Vector2(300, 300)
+var screen_size = DisplayServer.screen_get_size()
+const size_player = Vector2(10,10)
 
+func _ready() -> void:
+	position = start_position
 
-func _physics_process(delta: float) -> void:
-
-	var direction := Vector2(Input.get_axis("left", "right"),Input.get_axis("up", "down")).normalized()
+func _physics_process(_delta: float) -> void:
+	
+	var direction := Input.get_vector("left","right","up","down").normalized()
+	if position.x < size_player.x:
+		position.x = size_player.x
+		velocity.x = abs(velocity.x)
+	elif position.x > screen_size.x-size_player.x:
+		position.x = screen_size.x-size_player.x
+		velocity.x = -abs(-velocity.x)
+	if position.y < size_player.y:
+		position.y = size_player.y
+		velocity.y = abs(velocity.y)
+	elif position.y > screen_size.y-size_player.y:
+		position.y = screen_size.y-size_player.y
+		velocity.y = -abs(-velocity.y)
 	velocity = direction*SPEED
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i).get_collider()
+		
+		
+	
 
 	move_and_slide()
