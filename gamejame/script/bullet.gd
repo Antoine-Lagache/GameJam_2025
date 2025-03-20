37da48f830a529a8330af94cc246f8b2533a3f45
Nathanael Fortier <Nathanael.Fortier@grenoble-inp.org>
2025-03-20 17:08:32 +0100

    base projo

--- gamejame/script/bullet.gd
+++ gamejame/script/bullet.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
-var speed = 300.0
-var direction : float = 0.0
+var speed = 10.0 # nb pixel / seconde
+var direction : float = 0.5
 
 
 
@@ -9,4 +9,8 @@ func _ready():
 	pass
 	
 func _physics_process(delta: float) -> void:
-	pass
+	var direction_vector = Vector2(cos(direction),sin(direction))
+	global_position += speed*direction_vector
+	
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
