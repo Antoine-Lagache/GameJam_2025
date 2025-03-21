fa789e55eee5c142ed8ae1a92b488c3e94f7704c
Nathanael Fortier <Nathanael.Fortier@grenoble-inp.org>
2025-03-21 19:33:46 +0100

    ENFIIIIIIN DE LA VITESSE (shift pour ralentir)

--- gamejame/script/player.gd
+++ gamejame/script/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
+const SPEED = 500.0
 const start_position = Vector2(300, 300)
 var screen_size = DisplayServer.window_get_size()
 const size_player = Vector2(10,10)
@@ -29,6 +29,10 @@ func _physics_process(_delta: float) -> void:
 	elif position.y > screen_size.y-size_player.y:
 		position.y = screen_size.y-size_player.y
 		velocity.y = -abs(-velocity.y)
-	velocity = direction*SPEED
+	
+	if Input.is_key_pressed(KEY_SHIFT):
+		velocity = direction*SPEED/2
+	else:
+		velocity = direction*SPEED
 	
 	move_and_slide()
