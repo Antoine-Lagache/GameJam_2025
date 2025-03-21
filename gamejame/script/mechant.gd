33e3e4f8b392671ab90e5c0dde8c2db89e85a4d1
Nathanael Fortier <Nathanael.Fortier@grenoble-inp.org>
2025-03-21 18:25:50 +0100

    pattern propre

--- gamejame/script/mechant.gd
+++ gamejame/script/mechant.gd
@@ -1,33 +1,22 @@
 extends Node2D
 
 
-@onready var bullet = load("res://scene/bullet.tscn")
+@onready var pattern_path = load("res://scene/pattern.tscn")
 var bullet_limit:int = 100
 
 func _ready():
 	add_to_group("mechant")
 	var screen = DisplayServer.screen_get_size()
 	position = Vector2(screen.x/2, screen.y/2)
+	var pattern = pattern_path.instantiate()
+	add_child(pattern)
 	
 func _physics_process(_delta: float) -> void:
 	pass
 
 
 func _on_timer_timeout() -> void:
-	if len(get_tree().get_nodes_in_group("bullet")) < bullet_limit and Global.time_speed:
-		var angle = randf_range(0, 2 * PI)
-		for i in range(10):
-			var new_bullet:Node = bullet.instantiate()
-			new_bullet.speed = 600
-			new_bullet.direction =  2 * PI / 10 * i + angle
-			new_bullet.angular_velocity = 0.01
-			add_child(new_bullet)
-		for i in range(10):
-			var new_bullet:Node = bullet.instantiate()
-			new_bullet.speed = 600
-			new_bullet.direction =  2 * PI / 10 * (i + 0.5) + angle
-			new_bullet.angular_velocity = - 0.01
-			add_child(new_bullet)
+	pass
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
---
+++ gamejame/script/pattern.gd
@@ -0,0 +1,40 @@
+extends Node2D
+
+@onready var bullet = load("res://scene/bullet.tscn")
+var bullet_limit:int = 200
+var pattern_id:int = 1
+
+
+func _ready():
+	start_pattern(pattern_id)
+	
+func _physics_process(delta: float) -> void:
+	pass
+	
+func start_pattern(id:int) -> void:
+	pattern_id = id
+	match id:
+		0:
+			pass
+		1:
+			pattern_1()
+		_:
+			print("idk what happened, but you are trying to use a non existent pattern")
+			
+func pattern_1():
+	while(pattern_id == 1):
+		await get_tree().create_timer(0.5).timeout # on attend une demi seconde
+		if len(get_tree().get_nodes_in_group("bullet")) < bullet_limit and Global.time_speed:
+			var angle = randf_range(0, 2 * PI)
+			for i in range(10):
+				var new_bullet:Node = bullet.instantiate()
+				new_bullet.speed = 600
+				new_bullet.direction =  2 * PI / 10 * i + angle
+				new_bullet.angular_velocity = 0.01
+				add_child(new_bullet)
+			for i in range(10):
+				var new_bullet:Node = bullet.instantiate()
+				new_bullet.speed = 600
+				new_bullet.direction =  2 * PI / 10 * (i + 0.5) + angle
+				new_bullet.angular_velocity = - 0.01
+				add_child(new_bullet)
