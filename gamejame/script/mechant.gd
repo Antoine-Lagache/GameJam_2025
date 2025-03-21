8e447a46b8a2ea5961bf11350c6843f7e4002ae0
Nathanael Fortier <Nathanael.Fortier@grenoble-inp.org>
2025-03-21 20:01:57 +0100

    pattern 2 (spin2win)

--- gamejame/script/mechant.gd
+++ gamejame/script/mechant.gd
@@ -1,14 +1,15 @@
 extends Node2D
 
 
-@onready var pattern_path = load("res://scene/pattern.tscn")
+var pattern_path = load("res://scene/pattern.tscn")
 var bullet_limit:int = 100
+var pattern = pattern_path.instantiate()
 
 func _ready():
 	add_to_group("mechant")
 	var screen = DisplayServer.window_get_size()
 	position = Vector2(screen.x/2, screen.y/2)
-	var pattern = pattern_path.instantiate()
+	pattern.pattern_id=2
 	add_child(pattern)
 	
 func _physics_process(_delta: float) -> void:
@@ -16,6 +17,8 @@ func _physics_process(_delta: float) -> void:
 
 
 func _on_timer_timeout() -> void:
+	# Un peu wacky mais ça combine les deux pattern
+	# pattern.start_pattern(1+(pattern.pattern_id%2))
 	pass
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
--- gamejame/script/pattern.gd
+++ gamejame/script/pattern.gd
@@ -18,12 +18,14 @@ func start_pattern(id:int) -> void:
 			pass
 		1:
 			pattern_1()
+		2:
+			pattern_2()
 		_:
 			print("idk what happened, but you are trying to use a non existent pattern")
 			
 func pattern_1():
 	while(pattern_id == 1):
-		await get_tree().create_timer(1.0).timeout # on attend une demi seconde
+		await get_tree().create_timer(1.0).timeout
 		if len(get_tree().get_nodes_in_group("bullet")) < bullet_limit and Global.time_speed:
 			var angle = randf_range(0, 2 * PI)
 			for i in range(10):
@@ -38,3 +40,15 @@ func pattern_1():
 				new_bullet.direction =  2 * PI / 10 * (i + 0.5) + angle
 				new_bullet.angular_velocity = - 0.01
 				add_child(new_bullet)
+				
+func pattern_2():
+	var angle = randf_range(0, 2 * PI)
+	while(pattern_id == 2):
+		await get_tree().create_timer(1/(4*PI)).timeout # on veut faire un tour en une seconde
+		if len(get_tree().get_nodes_in_group("bullet")) < bullet_limit and Global.time_speed:
+			for i in range(3):
+				var new_bullet:Node = bullet.instantiate()
+				new_bullet.speed = 600
+				new_bullet.direction = angle + i*2*PI/3
+				add_child(new_bullet)
+			angle += 1/(4*PI)
