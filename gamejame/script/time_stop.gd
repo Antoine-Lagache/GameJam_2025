1c066576f02a45d6f829d3a354af007926ee9e22
Richard Galdeano <richard.galdeano@etu.univ-grenoble-alpes.fr>
2025-03-21 23:49:49 +0100

    Added sound when power appears on screen

--- gamejame/script/time_stop.gd
+++ gamejame/script/time_stop.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var power_apparition: AudioStreamPlayer2D = $Power_apparition
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	power_apparition.play()
 	add_to_group("time_stop")
 	
 
