e4adc699057166d16fd5ad39b1e5092bb8b2ab8b
Richard Galdeano <richard.galdeano@etu.univ-grenoble-alpes.fr>
2025-03-22 18:41:02 +0100

    Added defeat sound

--- gamejame/script/gameover.gd
+++ gamejame/script/gameover.gd
@@ -1,10 +1,14 @@
 extends Control
 
+@onready var defeat_sound: AudioStreamPlayer2D = $DefeatSound
 
+func _ready() -> void:
+	print('test')
+	defeat_sound.play()
 
 func _on_main_menu_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/start_menu.tscn")  # Replace with function body.
 
 
 func _on_quit_button_pressed() -> void:
-	get_tree().quit() # Replace with function body.
+	get_tree().quit()
