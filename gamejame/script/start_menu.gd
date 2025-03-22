f6309044a5a3d32ba078594fd6ec4e96c2637e60
Richard Galdeano <richard.galdeano@etu.univ-grenoble-alpes.fr>
2025-03-22 17:25:16 +0100

    Small rework of menu_buttons

--- gamejame/script/start_menu.gd
+++ gamejame/script/start_menu.gd
@@ -1,9 +1,12 @@
 extends Control
 
 
-
-func _on_game_button_pressed() -> void:
+func _on_start_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/main.tscn")  # Replace with function body.
+
+
+func _on_tutorial_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scene/tutorial.tscn")
 
 
 func _on_quit_button_pressed() -> void:
