eae099979567206321703cec3f440c1de49b9e16
Richard Galdeano <richard.galdeano@etu.univ-grenoble-alpes.fr>
2025-03-23 18:13:20 +0100

    Added animation on death

--- gamejame/script/player.gd
+++ gamejame/script/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var hero_hit: AudioStreamPlayer = $HeroHit
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 const SPEED = 500.0
 const start_position = Vector2(300, 300)
@@ -9,6 +10,7 @@ const size_player = Vector2(10,10)
 var i_frames = 30
 var mechant:Node
 var direction: Vector2
+var is_dead: bool = false
 
 
 func loose_pv():
@@ -19,10 +21,7 @@ func loose_pv():
 		flash_start()
 		$CanvasLayer/Health_bar.health += -1
 		if $CanvasLayer/Health_bar.health == 0:
-			if Global.niveau != 3:
-				get_tree().change_scene_to_file("res://scene/gameover.tscn")
-			else:
-				get_tree().change_scene_to_file("res://scene/gameover_lvl3.tscn")
+			animation_player.play("Dying_right")
 		i_frames = 30
 
 func heal():
@@ -48,35 +47,47 @@ func _ready() -> void:
 func _physics_process(_delta: float) -> void:
 	direction = Input.get_vector("left","right","up","down").normalized()
 	
-	if position.x < size_player.x:
-		position.x = size_player.x
-		velocity.x = abs(velocity.x)
-	elif position.x > screen_size.x-size_player.x:
-		position.x = screen_size.x-size_player.x
-		velocity.x = -abs(-velocity.x)
-	if position.y < size_player.y:
-		position.y = size_player.y
-		velocity.y = abs(velocity.y)
-	elif position.y > screen_size.y-size_player.y:
-		position.y = screen_size.y-size_player.y
-		velocity.y = -abs(-velocity.y)
-	elif Input.is_key_pressed(KEY_SHIFT):
-		velocity = direction*SPEED/2
+	if is_dead:
+		if (position.y < 1080):
+			position.y += 1080 * _delta
+			position.x += 150 * _delta
 	else:
-		velocity = direction*SPEED
-	
-	if direction.x <0:
-		$Sprite2D.flip_h  = false
-	else:
-		$Sprite2D.flip_h = true
-	
-	
+		if position.x < size_player.x:
+			position.x = size_player.x
+			velocity.x = abs(velocity.x)
+		elif position.x > screen_size.x-size_player.x:
+			position.x = screen_size.x-size_player.x
+			velocity.x = -abs(-velocity.x)
+		if position.y < size_player.y:
+			position.y = size_player.y
+			velocity.y = abs(velocity.y)
+		elif position.y > screen_size.y-size_player.y:
+			position.y = screen_size.y-size_player.y
+			velocity.y = -abs(-velocity.y)
+		elif Input.is_key_pressed(KEY_SHIFT):
+			velocity = direction*SPEED/2
+		else:
+			velocity = direction*SPEED
 		
-	if i_frames > 0:
-		i_frames -= 1
-	
-	move_and_slide()
+		if direction.x <0:
+			$Sprite2D.flip_h  = false
+		else:
+			$Sprite2D.flip_h = true
+			
+		if i_frames > 0:
+			i_frames -= 1
+		
+		move_and_slide()
 
 
 func _on_flash_timer_timeout() -> void:
 	$Sprite2D.material.set_shader_parameter("flash_modifier", 0.0)
+	
+func dying() -> void: #called by animation_player
+	is_dead = true
+	
+func game_is_over(): #called by animation_player
+	if Global.niveau != 3:
+		get_tree().change_scene_to_file("res://scene/gameover.tscn")
+	else:
+		get_tree().change_scene_to_file("res://scene/gameover_lvl3.tscn")
